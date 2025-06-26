ab1af3556c6b464e53d5cdf253a087a263338c4a
MRAGANK GAUTAM <145200189+Mragankk@users.noreply.github.com>
2025-06-26 21:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World from FastAPI on Vercel!"}
+
+
+# vercel app deployed on- https://fastapivercel-3flnuny2j-mragank-s-projects.vercel.app/
